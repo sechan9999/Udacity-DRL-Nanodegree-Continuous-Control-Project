b1eb0f63debf097c741f96083a1fbeaa241e0eef
Tyler Yang <0107wy@gmail.com>
2019-04-23 18:02:19 -0700

    Complete ReplayBuffer with fast SumTree implementation
    SumTree data structure makes the cost of most operations(add, sample, update) constant, and usage of vectorized operations with NumPy keeps the cost very low overall.
    
    Possibly due to numerical errors due to precision of floats, it sometime raises IndexError while sampling. This is handled by repeating the sample process whenever IndexError is raised.

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,5 +1,4 @@
 import math
-import time
 import numpy as np
 import torch
 from collections import deque, namedtuple
@@ -30,10 +29,6 @@ class ReplayBuffer:
         self.a = a
         self.separate_experiences = bool(separate_experiences)
 
-        self.sample_times = []
-        self.tensorize_times = []
-        self.update_times = []
-
         self._at_least_once_updated = False
 
         self.memory_write_idx = 0
@@ -44,12 +39,11 @@ class ReplayBuffer:
         self.priorities_a = np.zeros(buffer_size)
         self.tree = np.zeros(self._memory_start_idx + buffer_size) # starts from index 1, not 0; makes implementation easier and reduces many small computations
 
-        # self.memory = deque(maxlen=buffer_size)
-        # self.priorities_a = deque(maxlen=buffer_size)
         self.multistep_collectors = [deque(maxlen=n_multisteps) for _ in range(n_agents)]
         self.max_priority_a = 1.
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
 
+        self._divisors = np.power(2, np.arange(1, self._non_leaf_depth + 1)).reshape((-1, 1))
         self._discounts = np.power(self.gamma, np.arange(self.n_multisteps + 1))
         self._target_discount = float(self._discounts[-1])
 
@@ -67,19 +61,17 @@ class ReplayBuffer:
             tree_idx = self._memory_start_idx + self.memory_write_idx
             self.priorities_a[self.memory_write_idx] = self.max_priority_a
             self.tree[tree_idx] = self.max_priority_a
+            # tree_indices = np.floor_divide(tree_idx, self._divisors).reshape((-1,))
+            # np.add.at(self.tree, tree_indices, np.tile(delta_priority_a, self._non_leaf_depth))
             for _ in range(self._non_leaf_depth):
                 tree_idx = tree_idx // 2
                 self.tree[tree_idx] += delta_priority_a
-            if self.tree[1] < 0:
-                raise ValueError(self.tree[:10])
 
             self.memory_write_idx += 1
             if self.memory_write_idx >= self.buffer_size:
                 self._buffer_is_full = True
                 self.memory_write_idx = 0
 
-            # self.memory.append(tuple(collector))
-            # self.priorities_a.append(self.max_priority_a)
             if self.separate_experiences:
                 collector.clear()
         if done:
@@ -98,33 +90,25 @@ class ReplayBuffer:
             target_discount (float): discount factor for target max-Q value
             is_weights (torch.Tensor): tensor of importance-sampling weights
             indices (np.ndarray): sample indices"""
-        t0 = time.time()
-
-        sample_values = debug = np.linspace(0, self.tree[1], num=self.batch_size, endpoint=False, dtype=np.float32)
-        sample_values += np.multiply(np.random.rand(self.batch_size), np.subtract(sample_values[1], sample_values[0]))
-        tree_indices = np.ones(self.batch_size, dtype=np.int32)
-        for d in range(self._non_leaf_depth):
-            left_child_indices = np.multiply(tree_indices, 2)
-            right_child_indices = np.add(left_child_indices, 1)
-            greater_than_left = np.greater(sample_values, self.tree[left_child_indices])
-            sample_values = np.where(greater_than_left, np.subtract(sample_values, self.tree[left_child_indices]), sample_values)
-            tree_indices = np.where(greater_than_left, right_child_indices, left_child_indices)
-            if d == 19 and False:
-                raise Exception(sample_values, tree_indices)
+        indices_not_prepared = True
+        while indices_not_prepared:
+            sample_values = np.linspace(0, self.tree[1], num=self.batch_size, endpoint=False, dtype=np.float32)
+            sample_values += np.multiply(np.random.rand(self.batch_size), np.subtract(sample_values[1], sample_values[0]))
+            tree_indices = np.ones(self.batch_size, dtype=np.int32)
+            try:
+                for _ in range(self._non_leaf_depth):
+                    left_child_indices = np.multiply(tree_indices, 2)
+                    right_child_indices = np.add(left_child_indices, 1)
+                    greater_than_left = np.greater(sample_values, self.tree[left_child_indices])
+                    sample_values = np.where(greater_than_left, np.subtract(sample_values, self.tree[left_child_indices]), sample_values)
+                    tree_indices = np.where(greater_than_left, right_child_indices, left_child_indices)
+            except IndexError: # Don't know exactly why it occurs. Suspecting numerical error issues with floating numbers as a probable cause
+                continue
+            else:
+                indices_not_prepared = False
         indices = np.subtract(tree_indices, self._memory_start_idx)
 
-        # probs = np.divide(self.priorities_a, sum(self.priorities_a))
-
-        # indices = np.random.choice(len(self.memory), size=self.batch_size, replace=False, p=probs)
-
-        try:
-            experiences = tuple(zip(*[self.memory[i] for i in indices if self.memory[i] is not None]))
-        except:
-            raise Exception(debug)
-        if self.memory_write_idx > 1000:
-            raise Exception(indices)
-        self.sample_times.append(time.time() - t0)
-        t0 = time.time()
+        experiences = tuple(zip(*[self.memory[i] for i in indices if self.memory[i] is not None]))
 
         first_states = torch.tensor([e[0] for e in experiences[0]], dtype=torch.float, device=device)
         actions      = torch.tensor([e[1] for e in experiences[0]], dtype=torch.float, device=device)
@@ -139,10 +123,7 @@ class ReplayBuffer:
 
         is_weights = np.divide(self.priorities_a[indices], self.tree[1])
         is_weights = np.power(np.multiply(is_weights, self.buffer_size if self._buffer_is_full else self.memory_write_idx), -beta)
-        # is_weights = [probs[i] for i in indices if self.memory[i] is not None]
-        # is_weights = np.power(np.multiply(is_weights, len(self.memory)), -beta)
         is_weights = torch.tensor(np.divide(is_weights, np.max(is_weights)).reshape((-1, 1)), dtype=torch.float, device=device)
-        self.tensorize_times.append(time.time() - t0)
 
         return (first_states, actions, rewards, last_states, dones), self._target_discount, is_weights, indices
 
@@ -153,8 +134,9 @@ class ReplayBuffer:
         ======
             indices (array_like): indices of experience priorities to update
             new_priorities (array-like): new priority values for given indices"""
-        t0 = time.time()
-        new_priorities_a = np.power(new_priorities, self.a)
+        # Remove Duplicate Samples (discard except the first occurence in the array)
+        indices, idx_indices = np.unique(indices, return_index=True)
+        new_priorities_a = np.power(new_priorities[idx_indices], self.a)
 
         if not self._at_least_once_updated:
             self._at_least_once_updated = True
@@ -180,28 +162,16 @@ class ReplayBuffer:
                 idx_start //= 2
                 idx_end = ((idx_end - 1) // 2) + 1
 
-        debugs = {'indices': np.copy(indices), 'new_priorities': np.copy(new_priorities),
-                  'tree': np.copy(self.tree), 'priorities_a': np.copy(self.priorities_a),
-                  'memory_write_idx': self.memory_write_idx, 'memory_start_idx': self._memory_start_idx}
-        torch.save(debugs, 'debugs.var')
-        raise Exception()
+            self.tree[[0, 1]] = np.add(self.tree[2], self.tree[3])
 
         delta_priority_a = np.subtract(new_priorities_a, self.priorities_a[indices])
-        parent_indices = np.add(indices, self._memory_start_idx)
+        tree_indices = np.add(indices, self._memory_start_idx)
         self.priorities_a[indices] = new_priorities_a
-        self.tree[parent_indices] = new_priorities_a
-        for _ in range(self._non_leaf_depth - 1):
-            child_indices = parent_indices
-            parent_indices = np.floor_divide(child_indices, 2)
-            np.add.at(self.tree, parent_indices, delta_priority_a) # propagate changes in values to parent nodes
-            if np.any(self.tree < 0):
-                np.add.at(self.tree, parent_indices, -delta_priority_a)
-                raise RuntimeError(self.tree[parent_indices], parent_indices, child_indices, delta_priority_a)
-        self.tree[[0, 1]] = self.tree[2] + self.tree[3]
-        # for i, new_priority_a in zip(indices, new_priorities_a):
-        #     self.priorities_a[i] = float(new_priority_a)
+        self.tree[tree_indices] = new_priorities_a
+        tree_indices = np.floor_divide(tree_indices, self._divisors).reshape((-1,))
+        np.add.at(self.tree, tree_indices, np.tile(delta_priority_a, self._non_leaf_depth))
+
         self.max_priority_a = np.max(self.priorities_a)
-        self.update_times.append(time.time() - t0)
 
     def reset_multisteps(self, i_agent=-1):
         assert isinstance(i_agent, int) and -1 <= i_agent < self.n_agents
