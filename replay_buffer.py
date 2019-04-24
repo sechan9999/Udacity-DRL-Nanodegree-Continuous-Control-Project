c14ea5319bb38dba31fdfdbd239bc137c2fcc634
Tyler Yang <0107wy@gmail.com>
2019-04-23 22:25:08 -0700

    Remove unnecessary call of `np.linspace()` inside the loop
    This can be calculated before the loop and does not need to be re-calculated in the loop when an IndexError is raised.

--- replay_buffer.py
+++ replay_buffer.py
@@ -91,9 +91,9 @@ class ReplayBuffer:
             is_weights (torch.Tensor): tensor of importance-sampling weights
             indices (np.ndarray): sample indices"""
         indices_not_prepared = True
+        sample_value_basis = np.linspace(0, self.tree[1], num=self.batch_size, endpoint=False, dtype=np.float32)
         while indices_not_prepared:
-            sample_values = np.linspace(0, self.tree[1], num=self.batch_size, endpoint=False, dtype=np.float32)
-            sample_values += np.multiply(np.random.rand(self.batch_size), sample_values[1])
+            sample_values = np.add(sample_value_basis, np.multiply(np.random.rand(self.batch_size), sample_values[1]))
             tree_indices = np.ones(self.batch_size, dtype=np.int32)
             try:
                 for _ in range(self._non_leaf_depth):
